3dcbecb17f403f0ad527cc76efadff2596344b4c
Jens Gross <jens.gross.78@googlemail.com>
2026-09-07 18:25:47 +0200

    test: import what the startup test uses instead of spelling it out inline

--- src/test/java/org/courtside/shared/DatabaseTlsStartupTest.java
+++ src/test/java/org/courtside/shared/DatabaseTlsStartupTest.java
@@ -3,12 +3,14 @@ package org.courtside.shared;
 import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.Banner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -23,7 +25,7 @@ class DatabaseTlsStartupTest {
         // when
         Throwable failure = catchThrowable(() -> new SpringApplicationBuilder(VerifiedPool.class)
                 .web(WebApplicationType.NONE)
-                .bannerMode(org.springframework.boot.Banner.Mode.OFF)
+                .bannerMode(Banner.Mode.OFF)
                 .run("--courtside.database.tls.mode=verify-full"));
 
         // then
@@ -35,7 +37,7 @@ class DatabaseTlsStartupTest {
     }
 
     @Configuration(proxyBeanMethods = false)
-    @org.springframework.context.annotation.Import(DatabaseTlsConfiguration.class)
+    @Import(DatabaseTlsConfiguration.class)
     static class VerifiedPool {
 
         @Bean
